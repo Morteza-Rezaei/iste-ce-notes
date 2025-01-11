763c88828c269c2c10bc3446527a186ec5a224b8
Morteza <moradimurtaza84@gmail.com>
2025-01-11 23:19:12 +0300

    ntp: giriş

---
+++ ntp/java_syntax/src/Fonksiyonlar.java
@@ -0,0 +1,5 @@
+public class Fonksiyonlar {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ntp/java_syntax/src/MyClass.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+        // çıktı işlemler
+        // System.out.println("bir satır aşağı iner");
+        // System.out.print("satır devam eder");
+
+        // klavyeden bir değer alma işlemi
+        // Scanner tara = new Scanner(System.in);
+        // String metin = tara.next();
+        // double a1 = tara.nextDouble();
+        // float a2 = tara.nextFloat();
+        // int a3 = tara.nextInt();
+
+        // değişken tanımlama
+        // int sayi = 5;
+        // float ondalik_sayi = 5.3f;
+        // double ondalik_sayi2 = 5.3;
+        // char karakter = 'D';
+        // boolean booldeger = true;
+        // String metin_değer = "Hello";
+
+        // if else
+        // int sayi1 = 20;
+        // int sayi2 = 15;
+        // if (sayi1 > sayi2) {
+        // System.out.println("sayı1, sayı2'den büyüktür");
+        // } else if (sayi1 < sayi2) {
+        // System.out.println("sayı1, sayı2'den küçüktür");
+        // } else {
+        // System.out.println("sayı1, sayı2'ye eşittir");
+        // }
+
+        // switch-case
+        // int day = 4;
+        // switch (day) {
+        // case 6:
+        // System.out.println("Today is Saturday");
+        // break;
+        // case 7:
+        // System.out.println("Today is Sunday");
+        // break;
+        // default:
+        // System.out.println("Looking forward to the Weekend");
+        // }
+
+        // Ternary Operator
+        // variable declaration
+        // int n1 = 5, n2 = 10, max;
+        // System.out.println("First num: " + n1);
+        // System.out.println("Second num: " + n2);
+        // Largest among n1 and n2
+        // max = (n1 > n2) ? n1 : n2;
+        // Print the largest number
+        // System.out.println("Maximum is = " + max);
+
+        // for loop
+        // for (int i = 0; i < 5; i++) {
+        // System.out.print(i);
+        // }
+
+        // foreach loop
+        // String[] araba = { "Volvo", "BMW", "Ford", "Mazda" };
+        // for (String alinan : araba) {
+        // System.out.println(alinan);
+        // }
+
+        // continue and break
+        // for (int i = 0; i < 10; i++) {
+        // if (i == 4) {
+        // continue;
+        // } else if (i == 8) {
+        // break;
+        // }
+        // System.out.print(i);
+        // }
+
+        // while loop
+        // int i = 0;
+        // while (i < 5) {
+        // System.out.println(i);
+        // i++;
+        // }
+
+        // do-while loop
+        // int i = 0;
+        // do {
+        // System.out.println(i);
+        // i++;
+        // } while (i < 5);
+
+    }
+}
